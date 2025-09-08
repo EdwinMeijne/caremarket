cad0e171dd45cac72ebcde5ad87f0cc634463d04
Edwin Meijne <git@edwinmeijne.nl>
2025-09-08 14:18:23 +0200

    feat: add server action and simple frontend request form

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import CareRequestForm from "@/app/ui/new-care-request-form";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container mx-auto py-8">
+      <h1 className="text-2xl font-bold text-center mb-8">
+        Care Request System
+      </h1>
+      <CareRequestForm />
     </div>
   );
 }
